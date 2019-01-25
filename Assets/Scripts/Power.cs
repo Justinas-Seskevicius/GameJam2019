910a0db9d46e6e9fc1fc25951eba559c3193a208
zeFerret <justinas.seskevicius@gmail.com>
2019-01-26 01:58:57 +0200

    feat: 4 ui buttons with changing power names

---
+++ Assets/Scripts/Power.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Power : MonoBehaviour
+{
+    [SerializeField] string currentPower = "Tornado";
+
+    public void SetCurrentPower(string setPower)
+    {
+        currentPower = setPower;
+    }
+
+    public string GetSelectedPower()
+    {
+        return currentPower;
+    }
+
+    private void Update()
+    {
+        UsePower();
+    }
+
+    private void UsePower()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Debug.Log(Input.mousePosition + "  === Current power -> " + currentPower);
+        }
+    }
+}
---
+++ Assets/Scripts/PowerButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerButton : MonoBehaviour
+{
+    [SerializeField] string powerName;
+
+    private void OnMouseDown()
+    {
+        var buttons = FindObjectsOfType<PowerButton>();
+        foreach (PowerButton button in buttons)
+        {
+            button.GetComponent<MeshRenderer>().material.color = new Color32(89, 89, 89, 255);
+        }
+        GetComponent<MeshRenderer>().material.color = Color.white;
+        FindObjectOfType<Power>().SetCurrentPower(powerName);
+    }
+}
